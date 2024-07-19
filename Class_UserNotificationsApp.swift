15841d7e3d39448bc6dcf5415a0625a78fba8a64
Julia Morales <juliamoraless1204@gmail.com>
2024-07-19 16:43:44 -0300

    Initial commit

---
+++ Class_UserNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class_UserNotificationsApp.swift
+//  Class_UserNotifications
+//
+//  Created by Julia Morales on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Class_UserNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Class_UserNotifications
+//
+//  Created by Julia Morales on 19/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
